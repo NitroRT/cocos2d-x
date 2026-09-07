62ba79557bbf37ad87d656417f599c27c2338600
Vladislav Kochetkov <galefaxx@gmail.com>
2026-09-07 08:21:10 +0200

    Removing GLES 2.0

--- cocos/platform/android/CCGL-android.h
+++ cocos/platform/android/CCGL-android.h
@@ -26,46 +26,18 @@ THE SOFTWARE.
 #pragma once
 
 #define glClearDepth                glClearDepthf
-#define glDeleteVertexArrays        glDeleteVertexArraysOES
-#define glGenVertexArrays           glGenVertexArraysOES
-#define glBindVertexArray           glBindVertexArrayOES
-#define glMapBuffer                 glMapBufferOES
-#define glUnmapBuffer               glUnmapBufferOES
-
-#define GL_DEPTH24_STENCIL8         GL_DEPTH24_STENCIL8_OES
-#define GL_WRITE_ONLY               GL_WRITE_ONLY_OES
 
 // GL_GLEXT_PROTOTYPES isn't defined in glplatform.h on android ndk r7 
 // we manually define it here
-#include <GLES2/gl2platform.h>
+#include <GLES3/gl3platform.h>
 #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES 1
 #endif
 
 // normal process
-#include <GLES2/gl2.h>
+#include <GLES3/gl3.h>
 #include <GLES2/gl2ext.h>
-// gl2.h doesn't define GLchar on Android
-typedef char GLchar;
 // android defines GL_BGRA_EXT but not GL_BRGA
 #ifndef GL_BGRA
 #define GL_BGRA  0x80E1
 #endif
-
-// ETC2 is core from GLES 3 on and so is absent from the GLES 2 headers included above, while
-// glCompressedTexImage2D that consumes these is plain GLES 2 - only the enum values are missing.
-#ifndef GL_COMPRESSED_RGB8_ETC2
-#define GL_COMPRESSED_RGB8_ETC2       0x9274
-#endif
-#ifndef GL_COMPRESSED_RGBA8_ETC2_EAC
-#define GL_COMPRESSED_RGBA8_ETC2_EAC  0x9278
-#endif
-
-//declare here while define in EGLView_android.cpp
-extern PFNGLGENVERTEXARRAYSOESPROC glGenVertexArraysOESEXT;
-extern PFNGLBINDVERTEXARRAYOESPROC glBindVertexArrayOESEXT;
-extern PFNGLDELETEVERTEXARRAYSOESPROC glDeleteVertexArraysOESEXT;
-
-#define glGenVertexArraysOES glGenVertexArraysOESEXT
-#define glBindVertexArrayOES glBindVertexArrayOESEXT
-#define glDeleteVertexArraysOES glDeleteVertexArraysOESEXT
